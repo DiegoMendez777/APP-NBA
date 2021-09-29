45cec7b3a2df8a7a4968818fe9fb5c7d597f20ff
DiegoMendez777 <diego.mendez@iesjoandaustria.org>
2021-09-29 16:57:33 +0200

    Login Android

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSignIn = findViewById(R.id.btnSingIn);
+        EditText txtUserName = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        TextView lblLoginResult = findViewById(R.id.lblLoginResult);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (txtUserName.getText().toString().equals("123")) {
+                    Log.i("Test", "Login ok");
+                } else {
+                    Log.i("Test", "Login ko");
+                }
+            }
+        });
+    }
+}
+
