aca6a34e81413759da70c223d81b16cf78dfea79
DiegoMendez777 <diego.mendez@iesjoandaustria.org>
2021-10-04 19:20:11 +0200

    login

--- app/src/main/java/com/example/primeraapp/MainActivity.java
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -12,23 +12,29 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*declaration id Button*/
         Button btnSignIn = findViewById(R.id.btnSingIn);
+        /*declaration id EditText*/
         EditText txtUserName = findViewById(R.id.txtUsername);
         EditText txtPassword = findViewById(R.id.txtPassword);
+        /*declaration id Label*/
         TextView lblLoginResult = findViewById(R.id.lblLoginResult);
 
+        /*declaration when button is clicked*/
         btnSignIn.setOnClickListener(new View.OnClickListener() {
+            /*This checks if you log in with the user "admin" and password "admin", the label will tell you if you have logged in correctly*/
             public void onClick(View v) {
                 if (txtUserName.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
-                    lblLoginResult.setText("Loggued in");
-                    Log.i("Test", "Login ok");
+                    lblLoginResult.setText("Loggued in");/*Text on Label*/
+                    Log.i("Test", "Login ok");/*If the interaction with the click works, you will see an ok in the log*/
                 } else {
-                    Log.i("Test", "Login ko");
+                    Log.i("Test", "Login ko");/*if the interaction with the click does not work you will see a ko in the log*/
                 }
             }
         });
